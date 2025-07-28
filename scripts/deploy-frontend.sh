5a17c5af79108520d202dcfabbe4f252233878d4
NikeshDev-LF <nikeshdevkota@lftechnology.com>
2025-07-28 19:57:36 +0545

    testing frontkend deployment

---
+++ scripts/deploy-frontend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Move to frontend directory
+cd frontend
+
+# Install dependencies
+npm ci
+
+# Build the frontend
+npm run build
+
+# Deploy to S3
+aws s3 sync build/ s3://$S3_BUCKET_NAME --delete
+
+# Invalidate CloudFront cache
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
