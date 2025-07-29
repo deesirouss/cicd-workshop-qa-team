db79ec4c3c9725b077ff29cfc90a54c988c71d61
SujanManandhar <sujanmanandhar@lftechnology.com>
2025-07-29 15:07:29 +0545

    test

---
+++ scripts/deploy-frontend.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd frontend
+npm install
+npm run build
+aws s3 sync build/ s3://$S3_BUCKET_NAME --delete
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
