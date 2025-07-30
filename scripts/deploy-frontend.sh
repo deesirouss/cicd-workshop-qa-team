7336b1ef76bba8d1543a0b949af43bf59c18487e
SujanManandhar <sujanmanandhar@lftechnology.com>
2025-07-30 09:48:18 +0545

    test

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -4,6 +4,7 @@ branch_name="${GITHUB_REF#refs/heads/}"
 
 cd frontend
 npm install
+REACT_APP_API_URL=$REACT_APP_API_URL
 npm run build
 aws s3 sync build/ s3://$S3_BUCKET_NAME --delete
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
